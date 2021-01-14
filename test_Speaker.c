8aa6d08643856f595b9a8225f2146623d3209f28
ch-wannaporn <74587173+ch-wannaporn@users.noreply.github.com>
2021-01-14 23:17:55 +0700

    Add files via upload

---
+++ test_Speaker.c
@@ -0,0 +1,345 @@
+/*Base register adddress header file*/
+#include "stm32l1xx.h"
+/*Library related header files*/
+#include "stm32l1xx_ll_gpio.h"
+#include "stm32l1xx_ll_pwr.h"
+#include "stm32l1xx_ll_rcc.h"
+#include "stm32l1xx_ll_bus.h"
+#include "stm32l1xx_ll_utils.h"
+#include "stm32l1xx_ll_system.h"
+#include "stm32l1xx_ll_tim.h"
+#include "stm32l1xx_ll_lcd.h"
+#include "stm32l152_glass_lcd.h"
+
+void SystemClock_Config(void);
+void GPIO_Config(void);
+void TIMBase_Config(void);
+void TIM_BASE_NOTE_Config(void);
+void TIM_OC_GPIO_Config(void);
+void TIM_OC_Config(void);
+void showCount(uint8_t c);
+void reset7Seg(void);
+void setSpeaker(void);
+
+#define ON 1
+#define OFF 0
+
+#define E_O6					(uint16_t)1318
+#define MUTE					(uint16_t) 2
+
+/*for 10ms update event*/
+#define TIMx_PSC			2 
+
+/*Macro function for ARR calculation*/
+#define ARR_CALCULATE(N) ((32000000) / ((TIMx_PSC) * (N)))
+
+uint8_t i;
+uint8_t disp[3];
+uint8_t Green = ON;
+uint8_t Yellow = OFF;
+uint8_t Red = OFF;
+uint8_t input = 1;
+
+uint32_t seg[10]={LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14,
+									LL_GPIO_PIN_10 | LL_GPIO_PIN_11,
+									LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_15 | LL_GPIO_PIN_13 | LL_GPIO_PIN_12,
+									LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_15 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12,
+									LL_GPIO_PIN_14 | LL_GPIO_PIN_15 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11,
+									LL_GPIO_PIN_2 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12,
+									LL_GPIO_PIN_2 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 | LL_GPIO_PIN_13 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12,
+									LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11,
+									LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15,
+									LL_GPIO_PIN_2 | LL_GPIO_PIN_14 | LL_GPIO_PIN_10 | LL_GPIO_PIN_15 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12};
+
+uint32_t digit[4] = {LL_GPIO_PIN_0 , LL_GPIO_PIN_1 , LL_GPIO_PIN_2 , LL_GPIO_PIN_3};
+
+int main()
+{
+	SystemClock_Config();
+	GPIO_Config();
+	TIMBase_Config();
+	TIM_OC_Config();
+	
+	while(1)
+	{
+		if (Green == ON)
+		{
+			LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_11);
+			input = LL_GPIO_IsInputPinSet(GPIOD, LL_GPIO_PIN_2);
+			if(!input)
+			{
+				Green = OFF;
+				Yellow = ON;
+				i = 6;
+				input = 1;
+			}
+		}
+		
+		if (Yellow == ON)
+		{
+			LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_12);
+			showCount(i-1);
+			setSpeaker();
+			if(LL_TIM_GetCounter(TIM2)<500)
+				{
+					LL_TIM_EnableCounter(TIM4);
+				}
+			else
+				{
+					LL_TIM_DisableCounter(TIM4);
+				}
+			if (i == 6)
+			{
+				LL_TIM_EnableCounter(TIM2);
+			}
+			if(i == 0)
+			{
+				Yellow = OFF;
+				Red = ON;
+				i = 61;
+			}
+		}
+		
+		if (Red == ON)
+		{
+			LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_11);
+			showCount(i-1);
+			setSpeaker();
+			if(i == 0)
+			{
+				LL_TIM_DisableCounter(TIM2);
+				LL_TIM_DisableCounter(TIM4);
+				reset7Seg();
+				Red = OFF;
+				Green = ON;
+				LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12);				
+			}
+		}
+	}
+}
+
+void setSpeaker(){
+	if(LL_TIM_GetCounter(TIM2)<500)
+		{
+			LL_TIM_EnableCounter(TIM4);
+		}
+	else
+		{
+			LL_TIM_DisableCounter(TIM4);
+		}
+}
+
+void showCount(uint8_t c)
+{
+	for(uint8_t d = 0 ; d<4 ; ++d)
+	{
+		reset7Seg();
+		if(d==0)
+		{
+			
+		}
+		else if(d==1)
+		{
+			if(c/100 != 0)
+			{
+				LL_GPIO_SetOutputPin(GPIOB, seg[c/100]);
+				LL_GPIO_SetOutputPin(GPIOC, digit[d]);
+			}
+		}
+		else if(d==2)
+		{
+			if(!(c/10%10 == 0 && c/100 == 0))
+			{
+				LL_GPIO_SetOutputPin(GPIOB, seg[c/10%10]);
+				LL_GPIO_SetOutputPin(GPIOC, digit[d]);
+			}
+		}
+		else if(d==3)
+		{
+			LL_GPIO_SetOutputPin(GPIOB, seg[c%10]);
+			LL_GPIO_SetOutputPin(GPIOC, digit[d]);
+		}
+	}
+}
+
+void reset7Seg()
+{
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
+	LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15);
+}
+
+void GPIO_Config()
+{
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
+	
+	LL_GPIO_InitTypeDef GPIO_Init;
+	LL_GPIO_InitTypeDef ltc4727_initstruct;
+	LL_GPIO_InitTypeDef rgb_led;
+	
+	GPIO_Init.Mode = LL_GPIO_MODE_INPUT;
+	GPIO_Init.Pin = LL_GPIO_PIN_2;
+	GPIO_Init.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	LL_GPIO_Init(GPIOD, &GPIO_Init);
+	
+	ltc4727_initstruct.Mode = LL_GPIO_MODE_OUTPUT;
+	ltc4727_initstruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	ltc4727_initstruct.Pull = LL_GPIO_PULL_NO;
+	ltc4727_initstruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	ltc4727_initstruct.Pin = LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15;
+	LL_GPIO_Init(GPIOB, &ltc4727_initstruct);
+
+	ltc4727_initstruct.Pin = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3;
+	LL_GPIO_Init(GPIOC, &ltc4727_initstruct);
+	
+	rgb_led.Mode = LL_GPIO_MODE_OUTPUT;
+	rgb_led.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	rgb_led.Pin = LL_GPIO_PIN_10 |LL_GPIO_PIN_11 | LL_GPIO_PIN_12;
+	rgb_led.Pull = LL_GPIO_PULL_NO;
+	rgb_led.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	LL_GPIO_Init(GPIOC, &rgb_led);
+	
+	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12);
+}
+
+void TIMBase_Config(void)
+{
+	LL_TIM_InitTypeDef timbase_initstructure;
+	
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+	
+	timbase_initstructure.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
+	timbase_initstructure.CounterMode = LL_TIM_COUNTERMODE_UP;
+	timbase_initstructure.Autoreload = 1000-1;
+	timbase_initstructure.Prescaler = 32000-1;
+	
+	LL_TIM_Init(TIM2, &timbase_initstructure);
+	
+	LL_TIM_EnableIT_UPDATE(TIM2);
+	
+	NVIC_SetPriority(TIM2_IRQn, 0);
+	NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void TIM2_IRQHandler(void)
+{
+	if ((LL_TIM_IsActiveFlag_UPDATE(TIM2) == SET))
+	{
+		LL_TIM_ClearFlag_UPDATE(TIM2);
+		i--;
+	}
+}
+
+void TIM_BASE_NOTE_Config()
+{
+	LL_TIM_InitTypeDef timbase_initstructure;
+	
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM4);
+	//Time-base configure
+	timbase_initstructure.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
+	timbase_initstructure.CounterMode = LL_TIM_COUNTERMODE_UP;
+	timbase_initstructure.Autoreload = ARR_CALCULATE(E_O6) - 1;
+	timbase_initstructure.Prescaler =  TIMx_PSC - 1;
+	LL_TIM_Init(TIM4, &timbase_initstructure);
+}
+
+
+void TIM_OC_GPIO_Config(void)
+{
+	LL_GPIO_InitTypeDef gpio_initstructure;
+	
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+	
+	gpio_initstructure.Mode = LL_GPIO_MODE_ALTERNATE;
+	gpio_initstructure.Alternate = LL_GPIO_AF_2;
+	gpio_initstructure.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	gpio_initstructure.Pin = LL_GPIO_PIN_6;
+	gpio_initstructure.Pull = LL_GPIO_PULL_NO;
+	gpio_initstructure.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	LL_GPIO_Init(GPIOB, &gpio_initstructure);
+}
+
+void TIM_OC_Config()
+{
+	LL_TIM_OC_InitTypeDef tim_oc_initstructure;
+	
+	TIM_OC_GPIO_Config();
+	TIM_BASE_NOTE_Config();
+	
+	tim_oc_initstructure.OCState = LL_TIM_OCSTATE_DISABLE;
+	tim_oc_initstructure.OCMode = LL_TIM_OCMODE_PWM1;
+	tim_oc_initstructure.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
+	tim_oc_initstructure.CompareValue = LL_TIM_GetAutoReload(TIM4) / 2;
+	LL_TIM_OC_Init(TIM4, LL_TIM_CHANNEL_CH1, &tim_oc_initstructure);
+	/*Interrupt Configure*/
+	NVIC_SetPriority(TIM4_IRQn, 1);
+	NVIC_EnableIRQ(TIM4_IRQn);
+	LL_TIM_EnableIT_CC1(TIM4);
+	
+	/*Start Output Compare in PWM Mode*/
+	LL_TIM_CC_EnableChannel(TIM4, LL_TIM_CHANNEL_CH1);
+}
+
+void TIM4_IRQHandler(void)
+{
+	if(LL_TIM_IsActiveFlag_CC1(TIM4) == SET)
+	{
+		LL_TIM_ClearFlag_CC1(TIM4);
+	}
+}
+
+void SystemClock_Config(void)
+{
+  /* Enable ACC64 access and set FLASH latency */
+  LL_FLASH_Enable64bitAccess();;
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+
+  /* Set Voltage scale1 as MCU will run at 32MHz */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+ 
+  /* Poll VOSF bit of in PWR_CSR. Wait until it is reset to 0 */
+  while (LL_PWR_IsActiveFlag_VOSF() != 0)
+  {
+  };
+ 
+  /* Enable HSI if not already activated*/
+  if (LL_RCC_HSI_IsReady() == 0)
+  {
+    /* HSI configuration and activation */
+    LL_RCC_HSI_Enable();
+    while(LL_RCC_HSI_IsReady() != 1)
+    {
+    };
+  }
+ 
+
+  /* Main PLL configuration and activation */
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLL_MUL_6, LL_RCC_PLL_DIV_3);
+
+  LL_RCC_PLL_Enable();
+  while(LL_RCC_PLL_IsReady() != 1)
+  {
+  };
+ 
+  /* Sysclk activation on the main PLL */
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  };
+ 
+  /* Set APB1 & APB2 prescaler*/
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+
+  /* Set systick to 1ms in using frequency set to 32MHz                             */
+  /* This frequency can be calculated through LL RCC macro                          */
+  /* ex: __LL_RCC_CALC_PLLCLK_FREQ (HSI_VALUE, LL_RCC_PLL_MUL_6, LL_RCC_PLL_DIV_3); */
+  LL_Init1msTick(32000000);
+ 
+  /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
+  LL_SetSystemCoreClock(32000000);
+}
